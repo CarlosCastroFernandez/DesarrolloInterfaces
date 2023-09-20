c4f077dc6d8090871e60e321a7686c5d318d8d91
CarlosCastroFernandez <bubachico@gmail.com>
2023-09-20 11:34:30 +0200

    Calentando motores

---
+++ prueba3/src/main/java/Clases/PrimoUtilities.java
@@ -0,0 +1,27 @@
+package Clases;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class PrimoUtilities {
+
+    static Logger logger=Logger.getLogger(PrimoUtilities.class.getName());
+    public static Boolean esPrimo(Integer i) {
+        for (Integer n = 2; n < (i - 1); n++) {
+
+            if (i == 0) return false;
+
+        }
+        logger.info("Pimo encontrado "+i);
+        return true;
+    }
+
+
+    public static ArrayList<Integer> primoIntervalo(Integer inicial, Integer fin){
+        var salida=new ArrayList<Integer>();
+        for(Integer numero=inicial;numero<fin;numero++){
+            if(esPrimo(numero)) salida.add(numero);
+        }
+        return salida;
+    }
+}
--- prueba3/src/main/java/org/example/Main.java
+++ prueba3/src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import Clases.PrimoUtilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(PrimoUtilities.esPrimo(5));
+        System.out.println(PrimoUtilities.esPrimo(4));
+
+        var p=PrimoUtilities.primoIntervalo(1,10);
+
+
+        imprimir(p);
+
+    }
+
+    private static void imprimir(ArrayList<Integer> p) {
+        Iterator<Integer> it= p.iterator();
+        while(it.hasNext()){
+            Integer numero=it.next();
+            System.out.println(numero);
+        }
+        var params=new HashMap<String,String>();
+        params.put("hola","hola");
+        params.put("Carlos","Castro");
+        Iterator itn=params.entrySet().iterator();
+        while (itn.hasNext()){
+            Map.Entry nuevo= (Map.Entry) itn.next();
+            System.out.println(nuevo.getKey()+" "+nuevo.getValue());
+        }
     }
 }
